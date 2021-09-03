fd25fa147e80ccb041ee4da3458e909b9e972594
Carlos Martinez <curlosmart@gmail.com>
2021-09-03 11:45:20 -0400

    project complete

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = process.env.PORT || 8080
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end('<h1>Hello, World!</h1>')
+  console.log([req.url])
+
+  if ('/' === req.url) {
+    fs.readFile('index.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else if ('/about' === req.url) {
+    fs.readFile('about.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else if ('/contact-me' === req.url) {
+    fs.readFile('contact-me.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  } else {
+    fs.readFile('404.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      return res.end()
+    })
+  }
 })
 
 server.listen(port, () => {
